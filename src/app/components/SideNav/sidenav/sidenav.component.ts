fca3ab9f230dc4492d9abeaa3802160cdac5979b
Ade11edA <benchemam.adel@gmail.com>
2024-07-05 12:21:37 -0400

    Analysis, settings, and templates changes
    Reorganized the panels, settings has the proper grounds and inputs. Toggle button has been redesigned. Analysis buttons has been redesigned.

--- src/app/components/SideNav/sidenav/sidenav.component.ts
+++ src/app/components/SideNav/sidenav/sidenav.component.ts
@@ -1,56 +1,61 @@
 import { Component} from '@angular/core'
 
 interface Tab {
-    selected: boolean, 
-    label: string,
-    icon: string
+  selected: boolean,
+  label: string,
+  icon: string
 }
 
 
 @Component({
-    selector: 'app-sidenav',
-    templateUrl: './sidenav.component.html',
-    styleUrls: [ './sidenav.component.scss'],
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: [ './sidenav.component.scss'],
 
 })
 export class SidenavComponent {
 
-    tabs: Tab[] = [
-        {selected: false, label: 'Synthesis',icon: 'assets/sidenav/synthesize.svg'},
-        {selected: true, label: 'Edit',icon: 'assets/sidenav/edit.svg'},
-        {selected: false, label: 'Analysis', icon:'assets/sidenav/analyze.svg'},
-    ];
-    constructor(){
-    }
+  tabs: Tab[] = [
+    {selected: false, label: 'Synthesis',icon: 'assets/sidenav/synthesize.svg'},
+    {selected: true, label: 'Edit',icon: 'assets/sidenav/edit.svg'},
+    {selected: false, label: 'Analysis', icon:'assets/sidenav/analyze.svg'},
+  ];
+  constructor(){
+  }
 
 
-setCurrentTab(clickedTab: string){
+  setCurrentTab(clickedTab: string){
     this.tabs.forEach((tab)=>{
-        if(tab.label == clickedTab){
-            tab.selected = true;
-        } else{
-            tab.selected = false;
-        }
+      if(tab.label == clickedTab){
+        tab.selected = true;
+      } else{
+        tab.selected = false;
+      }
     });
-}
-isSelected(id: string): boolean{
-    let isSelected = false;
-    this.tabs.forEach((tab)=>{
-        if(tab.label == id){
-            isSelected = tab.selected;
-        }
+  }
+
+  isSelected(id: string): boolean {
+    return this.tabs.find(tab => tab.label === id)?.selected ?? false;
+  }
+  togglePanel(clickedTab: string): void {
+    this.tabs.forEach(tab => {
+      if (tab.label === clickedTab) {
+        tab.selected = !tab.selected; // Toggle selected state
+      } else {
+        tab.selected = false; // Close other panels
+      }
     });
-    return isSelected;
-}
-getSelected(): string {
+  }
+
+  getSelected(): string {
     let selectedTab = '';
     this.tabs.forEach((tab)=>{
-        if(tab.selected){
-            selectedTab = tab.label;
-        }
+      if(tab.selected){
+        selectedTab = tab.label;
+      }
     });
     return selectedTab;
-}
+  }
 
 
 }
