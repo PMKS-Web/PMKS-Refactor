d89c1d130bb84aaefeb46e2ecad6ffcc6167dd59
rkprad <rkprad@users.noreply.github.com>
2024-07-29 19:32:03 -0400

    Driver.js trial implementation and Naming Updates within Animation Bar
    Driver.js trial implementation carried out. The css is added to the index.html file and the rest of the script is in app.module.ts.
    
    The animationBar's play/speed controller and timeSlider names have been updated so that it can be captured better by the interactive tour.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatIconModule} from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
+import { driver } from "driver.js";
+
 /* Component Imports */
 import { InteractionDirective } from 'src/app/controllers/directives/interaction-directive';
 import { AppComponent } from './app.component';
@@ -150,4 +152,59 @@ import {MatSliderModule} from "@angular/material/slider";
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+/* Driver.js trial implementation */
+const driverObj = driver({
+  showProgress: true,
+  steps: [
+    {
+      popover: {
+        title: 'Welcome',
+        description: 'This pop-up tutorial will guide you various features of Planar Mechanism Kinematic Simulator Plus (PMKS+).'
+      }
+    },
+
+    {
+      element: '.side-nav',
+      popover: {
+        title: 'Usage Modes of PMKS+ ',
+        description: 'PMKS+ has three major usage modes: Synthesis, Edit and Analysis.',
+        side:"left",
+        align:'start'
+      }
+    },
+
+    {
+      element: '#animationBarPlaySpeed',
+      popover: {
+        title: 'Animation Controller ',
+        description: 'Animate any valid mechanisms and control the speed of animation using these buttons.',
+        side:"top",
+        align:"center"
+      }
+    },
+
+    {
+      element: '#animationBarTimeSlider',
+      popover: {
+        title: 'Time and Slider ',
+        description: 'Adjust the slider to display the position of the mechanism at that time step. Specific time step can also be specified in the textbox.',
+        side:"top",
+        align:"center"
+      }
+    },
+
+    {
+      element: '#viewButtons',
+      popover: {
+        title: 'Display Options',
+        description: 'Display the center of mass and labels of joints and links using these buttons. To adjust the size, zoom options are also available.',
+        side:"top",
+        align:"center"
+      }
+    },
+  ]
+});
+driverObj.drive();
