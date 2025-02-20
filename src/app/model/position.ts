f3278ed13bd9904855d485ef2094b605ec66d7a2
Javier DeLeon <30377331+Canpake59@users.noreply.github.com>
2025-02-20 13:59:26 -0500

    edited get angle and set angle for both link and position. should now correctly display angles from 0 to 360. in some extreme minor cases, sig figs go into huge places, but should rarely ever happen but if its a problem i can fix it.

--- src/app/model/position.ts
+++ src/app/model/position.ts
@@ -101,7 +101,7 @@ export class Position implements RigidBody {
   }
 
   get angle(): number {
-    return this._angle;
+    return (this._angle + 360) % 360;
   }
 
   // Setters
@@ -118,8 +118,8 @@ export class Position implements RigidBody {
     this.updateLocks(value);
   }
 
-  set angle(value) {
-    this._angle = value;
+  set angle(value: number) {
+    this._angle = (value % 360 + 360) % 360;
   }
   addTracer(newJoint: Joint) {
     this._joints.set(newJoint.id, newJoint);
