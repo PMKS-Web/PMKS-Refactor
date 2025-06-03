96327ac8947065df7a11361c0072bcd937bd3931
doravid <doravidmc@gmail.com>
2025-06-03 09:37:26 -0400

    Notification Panel

---
+++ src/app/components/Blocks/notification/notification.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NotificationService, Notification } from 'src/app/services/notification.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: "./notification.component.html",
+  styleUrls: ['./notification.component.scss']
+})
+export class NotificationComponent implements OnInit, OnDestroy {
+  notifications: Notification[] = [];
+  private subscription: Subscription = new Subscription();
+
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.notificationService.notifications$.subscribe(
+      notifications => this.notifications = notifications
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  closeNotification(id: string): void {
+    // Start fade out animation first
+    this.notificationService.startFadeOut(id);
+    
+    // Remove after animation completes
+    setTimeout(() => {
+      this.notificationService.removeNotification(id);
+    }, 300);
+  }
+
+  trackByFn(index: number, item: Notification): string {
+    return item.id;
+  }
+}
---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Notification {
+  id: string;
+  message: string;
+  timestamp: number;
+  fadingOut?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  private notificationsSubject = new BehaviorSubject<Notification[]>([]);
+  public notifications$ = this.notificationsSubject.asObservable();
+
+  showNotification(message: string): void {
+    const notification: Notification = {
+      id: this.generateId(),
+      message,
+      timestamp: Date.now(),
+      fadingOut: false
+    };
+
+    const currentNotifications = this.notificationsSubject.value;
+    this.notificationsSubject.next([...currentNotifications, notification]);
+    const fadeOutDurationMS = 5000;
+    // Start fade out after 9.7 seconds, then remove after 10 seconds
+    setTimeout(() => {
+      this.startFadeOut(notification.id);
+    }, fadeOutDurationMS - 400);
+
+    setTimeout(() => {
+      this.removeNotification(notification.id);
+    }, fadeOutDurationMS);
+  }
+
+  startFadeOut(id: string): void {
+    const currentNotifications = this.notificationsSubject.value;
+    const updatedNotifications = currentNotifications.map(n => 
+      n.id === id ? { ...n, fadingOut: true } : n
+    );
+    this.notificationsSubject.next(updatedNotifications);
+  }
+
+  removeNotification(id: string): void {
+    const currentNotifications = this.notificationsSubject.value;
+    const filteredNotifications = currentNotifications.filter(n => n.id !== id);
+    this.notificationsSubject.next(filteredNotifications);
+  }
+
+  private generateId(): string {
+    return Date.now().toString(36) + "-" + Math.random().toString(36).substr(2, 9);
+  }
+}
