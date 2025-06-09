7284bc799c4a225c2265926aae408a45d2ad139f
Doravid <doravidmc@gmail.com>
2025-06-09 14:58:43 -0400

    The positions in the sythesis bar now reflect where the reference point should be

--- src/app/components/SideNav/Synthesis/three-pos-synthesis/three-pos-synthesis.component.ts
+++ src/app/components/SideNav/Synthesis/three-pos-synthesis/three-pos-synthesis.component.ts
@@ -908,21 +908,15 @@ setPosYCoord(y: number, posNum: number){
   }
 
 getPosXCoord(posNum: number): number{
-    if(posNum==1)
-        return this.pos1X;
-    else if(posNum==2)
-        return this.pos2X;
-    else
-        return this.pos3X;
+    if(posNum==1) return this.getReferenceJoint(this.position1 as Position)._coords.x;
+    else if(posNum==2) return this.getReferenceJoint(this.position2 as Position)._coords.x;
+    return this.getReferenceJoint(this.position2 as Position)._coords.x;
 }
 
 getPosYCoord(posNum: number): number{
-    if(posNum==1)
-        return this.pos1Y;
-    else if(posNum==2)
-        return this.pos2Y;
-    else
-        return this.pos3Y;
+  if(posNum==1) return this.getReferenceJoint(this.position1 as Position)._coords.y;
+  else if(posNum==2) return this.getReferenceJoint(this.position2 as Position)._coords.y;
+  return this.getReferenceJoint(this.position2 as Position)._coords.y;
 }
 
 getPosAngle(posNum: number): number{
