68e3db7681e40a4b727f3abe4fd6f2f5e1063dac
Rodrick Moore <128014261+LightComplexx@users.noreply.github.com>
2025-09-18 12:28:45 -0400

    Undo/Redo Visual Bug Fix
    - When attempting to undo a change while the animation is playing, the animation will now revert to its previous state before the change was made and continue playing.
    - When attempting to redo a change, the animation will return to its most recent state after the change and continue playing.

--- src/app/components/ToolBar/undo-redo-panel/undo-redo-panel.component.ts
+++ src/app/components/ToolBar/undo-redo-panel/undo-redo-panel.component.ts
@@ -4,6 +4,8 @@ import { InteractionService } from 'src/app/services/interaction.service';
 import { Mechanism } from '../../../model/mechanism';
 import { NotificationService } from 'src/app/services/notification.service';
 import { UndoRedoService } from 'src/app/services/undo-redo.service';
+import { AnimationService } from 'src/app/services/animation.service';
+import {AnimationBarComponent} from "../../AnimationBar/animationbar/animationbar.component";
 
 @Component({
   selector: 'app-undo-redo-panel',
@@ -15,7 +17,10 @@ export class UndoRedoPanelComponent {
     public stateService: StateService,
     public undoRedoService: UndoRedoService,
     public mechanism: Mechanism,
-    private NotificationService: NotificationService
+    private NotificationService: NotificationService,
+
+    // Included to handle changes to animation
+    public animationService: AnimationService
   ) {}
   @HostListener('document:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
@@ -31,20 +36,68 @@ export class UndoRedoPanelComponent {
   // WHEN USER CLICKS UNDO
   onUndo(): void {
     console.log('Undo button clicked');
+    let animationBarRef: AnimationBarComponent = this.stateService.getAnimationBarComponent();
+    let animationWasPlaying: boolean = false;
+
+    // testing code for stopping animation
+    if(animationBarRef.getIsAnimating()){
+      // internal flag to replay animation later
+      if(!animationBarRef.getIsPausedAnimating()){
+        animationWasPlaying = true;
+      }
+
+      // stops animation
+      animationBarRef.controlAnimation('stop');
+
+      // clears trajectories from previous animation
+      this.stateService.getMechanism().clearTrajectories();
+    }
+    // end of test code
+
     // Call the undo function from UndoRedoService.
     this.undoRedoService.undo();
     this.mechanism.clearTrajectories();
     this.mechanism.notifyChange();
     this.NotificationService.showNotification('Undo Called');
+
+    // If an animation was playing before, start animation again.
+    if (animationWasPlaying){
+      // start animation
+      animationBarRef.controlAnimation('play');
+    }
   }
 
   //WHEN USER CLICKS REDO
   onRedo(): void {
     console.log('Redo button clicked');
+    let animationBarRef: AnimationBarComponent = this.stateService.getAnimationBarComponent();
+    let animationWasPlaying: boolean = false;
+
+    // testing code for stopping animation
+    if(animationBarRef.getIsAnimating()){
+      // internal flag to replay animation later
+      if(!animationBarRef.getIsPausedAnimating()){
+        animationWasPlaying = true;
+      }
+
+      // stops animation
+      animationBarRef.controlAnimation('stop');
+
+      // clears trajectories from previous animation
+      this.stateService.getMechanism().clearTrajectories();
+    }
+    // end of test code
+
     this.undoRedoService.redo();
     this.mechanism.clearTrajectories();
     this.mechanism.notifyChange();
     this.NotificationService.showNotification('Redo Called');
+
+    // If an animation was playing before, start animation again.
+    if (animationWasPlaying){
+      // start animation
+      animationBarRef.controlAnimation('play');
+    }
   }
 
   protected readonly StateService = StateService;
