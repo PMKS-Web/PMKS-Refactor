2139abe8d42b25475735ae29a082622895ba4614
Esther Kim <eykim1@wpi.edu>
2025-12-03 11:15:23 -0500

    currently can draw each individual link correctly in a welded link, so both two joint links and compound links. Does not handle intersections yet

--- src/app/services/svg-path.service.ts
+++ src/app/services/svg-path.service.ts
@@ -1,12 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Coord} from 'src/app/model/coord'
+import {Link} from "../model/link";
+import {UnitConversionService} from "./unit-conversion.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class SVGPathService {
 
-  constructor() {}
+  private scale = 1;
+
+  constructor(private unitConversionService: UnitConversionService) {};
 
   // Generates an SVG path string representing a single connection based on provided coordinates and radius.
   getSingleLinkDrawnPath(allCoords: Coord[], radius: number): string{
@@ -258,5 +262,238 @@ export class SVGPathService {
 
    return pathData;
   }
+
+  // checks if the path string is starting or ending a path; returns true if it is
+  isNewShape(pathString: string): boolean {
+    return pathString === '' || pathString.substring(pathString.length - 2) === 'Z ';
+  }
+
+  // used for creating a concave fillet between two lines
+  computeArcPointsAndRadius(line1: [Coord, Coord, Coord | null, Link], line2:[Coord, Coord, Coord | null, Link], arcRadius: number):[Coord, Coord, number] {
+    // modify line1 end point and line2 start point to create an arc between them
+    const arcOffset = Math.min(arcRadius, line1[0].getDistanceTo(line1[1]) / 2, line2[0].getDistanceTo(line2[1]) / 2);
+    const line1OffsetPoint:Coord = line1[0].clone()
+      .subtract(line1[1])
+      .normalize()
+      .multiply(arcOffset)
+      .add(line1[1]);
+
+    const line2OffsetPoint:Coord = line2[1].clone()
+      .subtract(line2[0])
+      .normalize()
+      .multiply(arcOffset)
+      .add(line2[0]);
+
+    // find angle between two lines
+    const line2Angle = Math.atan2(line2[1].y - line2[0].y, line2[1].x - line2[0].x);
+    const line1Angle = Math.atan2(line1[1].y - line1[0].y, line1[1].x - line1[0].x);
+    const angleBetweenLines = line2Angle - line1Angle;
+    const radius = arcOffset * Math.tan((Math.PI - angleBetweenLines) / 2);
+
+    return [line1OffsetPoint, line2OffsetPoint, radius];
+  }
+
+  // recalculates angle to be in between -pi and pi.
+  angleToPI(line1: [Coord, Coord, Coord | null, Link], line2:[Coord, Coord, Coord | null, Link]) {
+    // calculate angle of each individual line, given in radians
+    let line1Angle = Math.atan2(line1[1].y - line1[0].y, line1[1].x - line1[0].x);
+    let line2Angle = Math.atan2(line2[1].y - line2[0].y, line2[1].x - line2[0].x);
+
+    // angle in between the lines
+    let angle = line1Angle - line2Angle;
+
+    // recalculate the angle to be between -pi and pi
+    angle = angle % (2 * Math.PI);
+    if (angle > Math.PI) {
+      angle -= 2 * Math.PI;
+    }
+    if (angle < -Math.PI) {
+      angle += 2 * Math.PI;
+    }
+    return angle;
+  }
+
+  // This function returns the lines used to calculate and save the lines of a link
+  solveForExternalLines(subLinks: Map<number, Link>, radius: number): [Coord, Coord, Coord | null, Link][] {
+    // create a list of all the external lines of all the sublinks
+    /* First Coord is starting position
+      Second Coord is ending position
+      boolean is true if the line is an arc
+      Last is the center Coord of an arc, null if it is just a line
+    */
+    //radius = 0.15;
+    let externalLines: [Coord, Coord, Coord | null, Link][] = [];
+    let allLinkExternalLines: Map<Link, [Coord, Coord, Coord | null, Link][]> = new Map();
+
+    // find and add all external lines for sublinks
+    for (let link of subLinks.values()) {
+      let allCoords: Coord[] = [];
+      for(let joint of link.joints.values()) {
+        allCoords.push(joint._coords);
+      }
+      allCoords = Array.from(allCoords, (coord) => {
+        return this.unitConversionService.modelCoordToSVGCoord(coord);
+      });
+
+      let linkExternalLines: [Coord, Coord, Coord | null, Link][] = [];
+
+      // find the hull points using the coords
+      let hullPoints: Coord[] = this.grahamScan(allCoords);
+      let collinearCoords: Coord[] | undefined = this.findCollinearCoords(allCoords);
+
+      if (collinearCoords !== undefined) {
+        // Calculate perpendicular direction vectors for the line
+        const dirFirstToSecond = this.perpendicularDirection(collinearCoords[0], collinearCoords[1]);
+        const dirSecondToFirst = this.perpendicularDirection(collinearCoords[1], collinearCoords[0]);
+
+        // Line from first to second Point
+        const point1: Coord = new Coord(collinearCoords[0].x + dirFirstToSecond.x * radius, collinearCoords[0].y + dirFirstToSecond.y * radius);
+        const point2: Coord = new Coord(collinearCoords[1].x + dirFirstToSecond.x * radius, collinearCoords[1].y + dirFirstToSecond.y * radius);
+        externalLines.push([point1.clone(), point2.clone(), null, link]);
+        linkExternalLines.push([point1.clone(), point2.clone(), null, link]);
+
+        // Arc around first joint
+        const point3: Coord = new Coord(collinearCoords[1].x + dirSecondToFirst.x * radius, collinearCoords[1].y + dirSecondToFirst.y * radius);
+        externalLines.push([point2.clone(), point3.clone(), collinearCoords[1].clone(), link]);
+        linkExternalLines.push([point2.clone(), point3.clone(), collinearCoords[1].clone(), link]);
+
+        // Line from second back to first Point
+        const point4: Coord = new Coord(collinearCoords[0].x + dirSecondToFirst.x * radius, collinearCoords[0].y + dirSecondToFirst.y * radius);
+        externalLines.push([point3.clone(), point4.clone(), null, link]);
+        linkExternalLines.push([point3.clone(), point4.clone(), null, link]);
+
+        // Arc around the second joint
+        const point5: Coord = new Coord(collinearCoords[0].x + dirFirstToSecond.x * radius, collinearCoords[0].y + dirFirstToSecond.y * radius);
+        externalLines.push([point4.clone(), point5.clone(), collinearCoords[0].clone(), link]);
+        linkExternalLines.push([point4.clone(), point5.clone(), collinearCoords[0].clone(), link]);
+        //console.log(externalLines);
+      } else {
+        if (hullPoints.length < 3) {
+          throw new Error('At least three points are required to create a path with rounded corners.');
+        }
+
+        console.log("hull points");
+        console.log(hullPoints);
+
+        // Start the path, moving the pointer to the first correct point
+        const dirFirstToSecondInit = this.perpendicularDirection(hullPoints[0], hullPoints[1]);
+
+        // last position
+        let lastPosition: Coord = new Coord(hullPoints[0].x + dirFirstToSecondInit.x * radius, hullPoints[0].y + dirFirstToSecondInit.y * radius);
+
+        //iterate over all the points, one line and one arc at a time
+        for (let i = 0; i < hullPoints.length; i++) {
+          //get current points to look at.
+          const c0 = hullPoints[i];
+          const c1 = hullPoints[(i + 1) % hullPoints.length];
+          const c2 = hullPoints[(i + 2) % hullPoints.length];
+          //get the Perpendicular Direction for the Path
+          const dirFirstToSecond = this.perpendicularDirection(c0, c1);
+          const dirSecondToThird = this.perpendicularDirection(c1, c2);
+
+          // Line from first joint to second joint
+          let point1: Coord = new Coord(c1.x + dirFirstToSecond.x * radius, c1.y + dirFirstToSecond.y * radius);
+          externalLines.push([lastPosition, point1, null, link]);
+          linkExternalLines.push([lastPosition, point1, null, link]);
+
+          // Arc around second joint
+          let point2: Coord = new Coord(c1.x+ dirSecondToThird.x * radius, c1.y + dirSecondToThird.y * radius);
+          let point2Center: Coord = c1.clone();
+          externalLines.push([point1, point2, point2Center, link]);
+          linkExternalLines.push([point1, point2, point2Center, link]);
+          lastPosition = point2.clone();
+        }
+      }
+
+      allLinkExternalLines.set(link, linkExternalLines);
+    }
+
+    return externalLines;
+  }
+
+  calculateCompoundPath(subLinks: Map<number, Link>, allCoordsList: Coord[], r: number) {
+    // allExternalLines will hold the path coordinates for each line and arc for all the coords
+    let allExternalLines: [Coord, Coord, Coord | null, Link][] = this.solveForExternalLines(subLinks, r);
+    //console.log(allExternalLines);
+
+    if (allExternalLines.length < 1) {
+      return '';
+    }
+
+    // converting external lines list into a set, which we will delete lines we have used
+    const externalLinesSet = new Set(allExternalLines);
+
+    let pathString = '';
+
+    let timeoutCounter = 1000;
+
+    while (externalLinesSet.size > 1) {
+      // this is the first line from the set
+      let currentLine: [Coord, Coord, Coord | null, Link] = externalLinesSet.values().next().value;
+
+      let beginningPoint: Coord = currentLine[1].clone();
+
+      while (!currentLine[1].equals(beginningPoint, 1) || this.isNewShape(pathString)) {
+        if (timeoutCounter-- < 0) {
+          return pathString;
+        }
+
+        // find the next line that starts at the end of the current line
+        const nextLine = [...externalLinesSet].find((line) => {
+          return line[0].looselyEquals(currentLine[1], 1);
+        });
+        //console.log(nextLine);
+
+        if (!nextLine) {
+          break;
+        }
+        externalLinesSet.delete(nextLine);
+
+        // when there are two lines intersecting, create a fillet between them
+        if (currentLine[2] === null && nextLine[2] == null &&
+          // checking if angle between the two lines is greater than 10 degrees
+          Math.abs(this.angleToPI(nextLine, currentLine)) > (10 * Math.PI / 180)) {
+          let [currentLineOffsetPoint, nextLineOffsetPoint, radius] = this.computeArcPointsAndRadius(currentLine, nextLine, 1);
+
+          currentLine[1] = currentLineOffsetPoint;
+          nextLine[0] = nextLineOffsetPoint;
+
+          if (this.isNewShape(pathString)) {
+            pathString += 'M ' + currentLine[1].x + ', ' + currentLine[1].y + ' ';
+          } else {
+            pathString = this.pathStringForLine(currentLine, pathString);
+          }
+
+          // !!! is 0 0 0 correct?
+          pathString += 'A ' + radius + ', ' + radius + ' 0 0 0 ' + nextLine[0].x + ', ' + nextLine[0].y + ' ';
+        } else {
+          // otherwise, just draw a line between the two points
+          if (this.isNewShape(pathString)) {
+            pathString += 'M ' + currentLine[1].x + ', ' + currentLine[1].y + ' ';
+          } else {
+            pathString = this.pathStringForLine(currentLine, pathString);
+          }
+        }
+        currentLine = nextLine;
+      }
+      pathString = this.pathStringForLine(currentLine, pathString);
+      pathString += 'Z ';
+    }
+    //console.log(pathString);
+    return pathString;
+  }
+
+  // calculate and return the path string plus the path of the line
+  pathStringForLine(line: [Coord, Coord, Coord | null, Link], pathString: string):string {
+    if (line[2] !== null) {
+      // if the line is an arc
+      pathString += 'A ' + line[0].getDistanceTo(line[2]) + ', ' + line[0].getDistanceTo(line[2]) + ' 0 0 1 ' + line[1].x + ', ' + line[1].y + ' ';
+    } else {
+      // if the line is just a line
+      pathString += 'L ' + line[1].x + ', ' + line[1].y + ' ';
+    }
+    return pathString;
+  }
+
 
 }
