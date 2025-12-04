858b93030499c917440f59a5d1719d2c4025a702
Esther Kim <eykim1@wpi.edu>
2025-12-04 12:27:36 -0500

    added the intersection methods with for line/line, line/arc, arc/arc, and helper functions line/circle and circle/circle

--- src/app/services/svg-path.service.ts
+++ src/app/services/svg-path.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {Coord} from 'src/app/model/coord'
 import {Link} from "../model/link";
 import {UnitConversionService} from "./unit-conversion.service";
+import {arc} from "d3-shape";
+import {intersection} from "d3";
 
 @Injectable({
   providedIn: 'root',
@@ -313,6 +315,344 @@ export class SVGPathService {
     return angle;
   }
 
+  // checks if two numbers are loosely equal, bounded by a delta
+  twoNumsLooselyEquals(a: number, b: number, delta: number = 0.00001): boolean {
+    return Math.abs(a - b) <= delta;
+  }
+
+  // checks if a point is on a line
+  // return true if the point is on the line segment defined by lineStart and lineEnd
+  // return false otherwise
+  isPointOnLine(point: Coord, lineStart: Coord, lineEnd: Coord): boolean {
+    // check if the point is within a small range of the line segment.
+    let range = 0.00001;
+    if (
+      point.x < Math.min(lineStart.x, lineEnd.x) - range ||
+      point.x > Math.max(lineStart.x, lineEnd.x) + range ||
+      point.y < Math.min(lineStart.y, lineEnd.y) - range ||
+      point.y > Math.max(lineStart.y, lineEnd.y) + range
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  // checks if a point is on an arc
+  // return true if the point is within the arc
+  // return false if the point is outside the circle or if the point is on the circle, but not within the arc
+  isPointInArc(intersection: Coord, arcStart: Coord, arcEnd: Coord): boolean {
+    // the arc always goes from the start point to the end point in a counter-clockwise direction.
+    // use the cross product to determine if the point is on the left or right side of the line from the start point to the end point.
+    // if the point is on the left side, then it is within the arc.
+    // if the point is on the right side, then it is outside the arc.
+    let crossProduct =
+      (arcEnd.x - arcStart.x) * (intersection.y - arcStart.y) -
+      (intersection.x - arcStart.x) * (arcEnd.y - arcStart.y);
+
+    // check if the point is on the left side of the line from the start point to the end point
+    return crossProduct < 0;
+  }
+
+  // https://stackoverflow.com/questions/13937782/calculating-the-point-of-intersection-of-two-lines
+  // line intercept math by Paul Bourke http://paulbourke.net/geometry/pointlineplane/
+  // Determine the intersection point of two line segments
+  // Return undefiend if the lines don't intersect
+  lineLineIntersect(line1Start: Coord, line1End: Coord, line2Start: Coord, line2End: Coord): Coord | undefined {
+    let x1 = line1Start.x;
+    let y1 = line1Start.y;
+    let x2 = line1End.x;
+    let y2 = line1End.y;
+    let x3 = line2Start.x;
+    let y3 = line2Start.y;
+    let x4 = line2End.x;
+    let y4 = line2End.y;
+
+    let delta = 0.
+
+    // check if none of the lines are of length 0
+    if ((this.twoNumsLooselyEquals(x1, x2) && this.twoNumsLooselyEquals(y1, y2)) || (this.twoNumsLooselyEquals(x3, x4) && this.twoNumsLooselyEquals(y3, y4))) {
+      return undefined;
+    }
+
+    let denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+    // check if lines are parallel
+    if (denominator === 0) {
+      return undefined;
+    }
+
+    let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+    let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+
+    // check if intersection is not within the segments
+    if (ua <= 0 || ua >= 1 || ub <= 0 || ub >= 1) {
+      return undefined;
+    }
+
+    // return Coord with x and y coordinates of intersection
+    let x = x1 + ua * (x2 - x1);
+    let y = y1 + ua * (y2 - y1);
+
+    let intersection = new Coord(x, y);
+
+    // checks if the intersection is an end point of the segments
+    if (intersection.looselyEquals(line1Start, this.scale) ||
+    intersection.looselyEquals(line1End, this.scale) ||
+    intersection.looselyEquals(line2Start, this.scale) ||
+    intersection.looselyEquals(line2End, this.scale)) {
+      return undefined;
+    }
+
+    return intersection;
+  }
+
+  // return the intersection points between a line and a circle.
+  // if the line is tangent to the circle, then return the point of tangency.
+  // if the line does not intersect the circle, return undefined.
+  lineCircleIntersect(lineStart: Coord, lineEnd: Coord, circleCenter: Coord, circleRadius: number): Coord[] | undefined {
+    // check if the line is vertical or not
+    if (this.twoNumsLooselyEquals(lineEnd.x, lineStart.x)) {
+      // line is vertical so equation is x = c
+      let c = lineStart.x;
+
+      // find equation of the circle in the form (x - h)^2 + (y - k)^2 = r^2
+      let h = circleCenter.x;
+      let k = circleCenter.y;
+      let r = circleRadius;
+
+      // substitute x = c into the circle equation and solve for y
+      // this will give a quadratic equation in the form ay^2 + by + c = 0
+      let a = 1; // coefficient of y^2
+      let b = -2 * k; // coefficient of y
+      let d = c - h; // constant term divided by 2
+      let e = d * d + k * k - r * r; // constant term
+
+      // find the discriminant of the quadratic equation
+      // this will determine how many solutions there are
+      let D = b * b - 4 * a * e; // discriminant
+
+
+      if (D < 0) {
+        // if D is negative, then there are no real solutions and the line does not intersect the circle
+        return undefined;
+      } else if (D === 0) {
+        // if D is zero, then there is one real solution and the line is tangent to the circle
+        let y = -b / (2 * a); // solution for y
+        return [new Coord(c, y)]; // return the point of tangency as an array of one coordinate object
+      } else if (D > 0) {
+        // if D is positive, then there are two real solutions and the line intersects the circle at two points
+        let y1 = (-b + Math.sqrt(D)) / (2 * a); // first solution for y
+        let y2 = (-b - Math.sqrt(D)) / (2 * a); // second solution for y
+        return [new Coord(c, y1), new Coord(c, y2)]; // return the intersection points as an array of two coordinate objects
+      }
+    } else {
+      let intersections: Coord[] = [];
+      let slope = (lineStart.y - lineEnd.y) / (lineStart.x - lineEnd.x);
+      let y_intercept = lineStart.y - slope * lineStart.x;
+
+      let a = 1 + slope * slope;
+      let b = 2 * slope * (y_intercept - circleCenter.y) - 2 * circleCenter.x;
+      let c =
+        circleCenter.x * circleCenter.x +
+        (y_intercept - circleCenter.y) * (y_intercept - circleCenter.y) -
+        circleRadius * circleRadius;
+
+      let discriminant = b * b - 4 * a * c;
+      const tolerance = 0.00001;
+      if (discriminant < -tolerance) {
+        // line doesn't touch circle
+        return undefined;
+      } else if (discriminant < tolerance) {
+        // line is tangent to circle
+        let x = -b / (2 * a);
+        let y = slope * x + y_intercept;
+        intersections.push(new Coord(x, y));
+        return intersections;
+      } else {
+        // line intersects circle in two places
+        let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let y1 = slope * x1 + y_intercept;
+        intersections.push(new Coord(x1, y1));
+        let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        let y2 = slope * x2 + y_intercept;
+        intersections.push(new Coord(x2, y2));
+        return intersections;
+      }
+    }
+    return undefined;
+  }
+
+  // return the first intersection point between a line and an arc. The first is the one closest to the lineStart point.
+  // if the line is tangent to the arc, then return the point of tangency closest to the lineStart point.
+  // if the line does not intersect the arc, return undefined.
+  lineArcIntersect(lineStart: Coord, lineEnd: Coord, arcStart: Coord, arcEnd: Coord, arcCenter: Coord, findIntersectionCloseTo: Coord, arcRadius: number): Coord | undefined {
+    // find the intersection points between the line and the circle defined by the arc
+    let intersections: Coord[] | undefined = this.lineCircleIntersect(lineStart, lineEnd, arcCenter, arcRadius);
+
+    intersections = intersections?.filter((intersection) => {
+      return this.isPointOnLine(intersection, lineStart, lineEnd);
+    });
+
+    if (intersections === undefined || intersections.length === 0) {
+      return undefined;
+    }
+
+    // check if the intersection points are within the arc
+    let closestIntersection: Coord | undefined;
+    for (let intersection of intersections) {
+      if (
+        this.isPointInArc(intersection, arcStart, arcEnd) &&
+        !intersection.equals(arcStart, this.scale) &&
+        !intersection.equals(arcEnd, this.scale)
+      ) {
+        // if it is, return closest intersection point
+        if (closestIntersection === undefined) {
+          closestIntersection = intersection;
+        } else if (
+          closestIntersection.getDistanceTo(findIntersectionCloseTo) >
+          intersection.getDistanceTo(findIntersectionCloseTo)
+        ) {
+          closestIntersection = intersection;
+        }
+      }
+    }
+    return closestIntersection;
+  }
+
+  // return the intersection points between two circles.
+  // if the circles do not intersect, return undefined.
+  circleCircleIntersect(center: Coord, radius: number, center2: Coord, radius2: number): [Coord[] | undefined, boolean] {
+    let x1 = center.x;
+    let y1 = center.y;
+    let x2 = center2.x;
+    let y2 = center2.y;
+
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    let d = Math.sqrt(dx * dx + dy * dy);
+
+    // Circles are separate
+    if (d > radius + radius2) {
+      return [undefined, false];
+    }
+
+    // One circle is contained within the other
+    if (d < Math.abs(radius - radius2)) {
+      return [undefined, false];
+    }
+
+    // Circles are coincident
+    if (d === 0 && radius === radius2) {
+      // console.log('Circles are coincident');
+      return [undefined, true];
+    }
+
+    let a = (radius * radius - radius2 * radius2 + d * d) / (2 * d);
+    let h = Math.sqrt(radius * radius - a * a);
+    let x3 = x1 + (a * dx) / d;
+    let y3 = y1 + (a * dy) / d;
+    let x4 = x3 + (h * dy) / d;
+    let y4 = y3 - (h * dx) / d;
+    let x5 = x3 - (h * dy) / d;
+    let y5 = y3 + (h * dx) / d;
+
+    return [[new Coord(x4, y4), new Coord(x5, y5)], false];
+  }
+
+  // return the first intersection point between two arcs, the one closest to the startPosition point
+  // if the arcs are tangent, then return the point of tangency closest to the startPosition point
+  // if the arcs do not intersect, return undefined
+  arcArcIntersect(startPosition: Coord, endPosition: Coord, center: Coord, startPosition2: Coord, endPosition2: Coord, center2: Coord, radius: number): Coord | undefined {
+    // find the intersection points between the two circles defined by the arcs
+    let [intersections, coincident]: [Coord[] | undefined, boolean] = this.circleCircleIntersect(center, radius, center2, radius);
+
+    if (coincident) {
+      // circles are coincident, so we need to check if the arcs intersect
+      // check if the start and end points of the arcs are within the other arc
+
+      let allImportantIntersections: Coord[] = [];
+
+      if (this.isPointInArc(startPosition, startPosition2, endPosition2)) {
+        allImportantIntersections.push(startPosition);
+      }
+      if (this.isPointInArc(endPosition, startPosition2, endPosition2)) {
+        allImportantIntersections.push(endPosition);
+      }
+      if (this.isPointInArc(startPosition2, startPosition, endPosition)) {
+        allImportantIntersections.push(startPosition2);
+      }
+      if (this.isPointInArc(endPosition2, startPosition, endPosition)) {
+        allImportantIntersections.push(endPosition2);
+      }
+
+      // if there are no intersections, then the arcs do not intersect.
+      if (allImportantIntersections.length === 0) {
+        return undefined;
+      }
+
+      // else find the intersection closest to the startPosition.
+      let closestIntersection: Coord | undefined;
+      for (let intersection of allImportantIntersections) {
+        if (closestIntersection === undefined) {
+          closestIntersection = intersection;
+        } else if (
+          intersection.getDistanceTo(startPosition) < closestIntersection.getDistanceTo(startPosition)
+        ) {
+          closestIntersection = intersection;
+        }
+      }
+      return closestIntersection;
+    }
+
+    // check if no intersections
+    if (intersections === undefined) {
+      return undefined;
+    }
+
+    // check if intersection points are within the arcs
+    let closestIntersection: Coord | undefined;
+
+    for (let intersection of intersections) {
+      if (
+        this.isPointInArc(intersection, startPosition, endPosition) &&
+        this.isPointInArc(intersection, startPosition2, endPosition2) &&
+        !intersection.equals(startPosition, this.scale) &&
+        !intersection.equals(endPosition, this.scale) &&
+        !intersection.equals(startPosition2, this.scale) &&
+        !intersection.equals(endPosition2, this.scale)
+      ) {
+        if (!closestIntersection) {
+          // for first iteration only
+          closestIntersection = intersection;
+        } else if (intersection.getDistanceTo(startPosition) < closestIntersection.getDistanceTo(startPosition)) {
+          closestIntersection = intersection;
+        }
+      }
+    }
+
+    return closestIntersection;
+  }
+
+  // finds intersection between line/arc and line/arc
+  intersectsWith(line1: [Coord, Coord, Coord | null, Link], line2: [Coord, Coord, Coord | null, Link], radius: number): Coord | undefined {
+    if (line1[2] === null && line2[2] === null) {
+      // line and line intersection
+      return this.lineLineIntersect(line1[0], line1[1], line2[0], line2[1]);
+    } else if (line1[2] === null && line2[2] !== null) {
+      // line and arc intersection
+      return this.lineArcIntersect(line1[0], line1[1], line2[0], line2[1], line2[2], line1[0], radius);
+    } else if (line1[2] !== null && line2[2] === null) {
+      // arc and line intersection
+      return this.lineArcIntersect(line2[0], line2[1], line1[0], line1[1], line1[2], line1[0], radius);
+    } else if (line1[2] !== null && line2[2] !== null) {
+      // arc and arc intersection
+      return this.arcArcIntersect(line1[0], line1[1], line1[2], line2[0], line2[1], line2[2], radius);
+    } else {
+      // error
+      return undefined;
+    }
+  }
+
   // This function returns the lines used to calculate and save the lines of a link
   solveForExternalLines(subLinks: Map<number, Link>, radius: number): [Coord, Coord, Coord | null, Link][] {
     // create a list of all the external lines of all the sublinks
@@ -406,6 +746,39 @@ export class SVGPathService {
       }
 
       allLinkExternalLines.set(link, linkExternalLines);
+    }
+
+    // need to process the external lines we got
+    let stopCounter = 10000;
+
+    // will hold the intersection points of all external lines
+    // x is the x coord of intersection, y is y coord of intersection, and i is index in external lines array
+    let allIntersectionPoints: {point: Coord, i: number}[] = [];
+
+    // for each external line, need to check for intersections with all other external lines
+    for (let i = 0; i < externalLines.length - 1; i++) {
+      const line1: [Coord, Coord, Coord | null, Link] = externalLines[i];
+      for (let j = i + 1; j < externalLines.length; j++) {
+        if (stopCounter-- < 0) {
+          console.error("Error, stop counter is at 0");
+          return [];
+        }
+
+        const line2: [Coord, Coord, Coord | null, Link] = externalLines[j];
+
+        // check if lines intersect, if they do save the intersections
+        const intersection = this.intersectsWith(line1, line2, radius);
+
+        if (intersection !== undefined) {
+          if (!(line1[1].equals(intersection, this.scale) || line1[0].equals(intersection, this.scale))) {
+            allIntersectionPoints.push({point: intersection, i: i});
+          }
+          if (!(line2[0].equals(intersection, this.scale) || line2[1].equals(intersection, this.scale))) {
+            allIntersectionPoints.push({point: intersection, i: j});
+          }
+        }
+
+      }
     }
 
     return externalLines;
